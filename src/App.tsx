c20aa96da7ebd6335f72a0c92f1d67304a38328f
Fabio <binhorh@hotmail.com>
2026-06-08 00:25:54 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <div className="min-h-screen">
+
+        <h1
+          className="
+          text-center
+          text-4xl
+          font-bold
+          mt-20
+          "
+        >
+          ShirtStore
+        </h1>
+
+      </div>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
